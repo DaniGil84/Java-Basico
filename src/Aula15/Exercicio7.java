ae7b1ed6de70c97243d69f77d9f5d13ca9d3ae59
DaniGil84 <daniela.angela.gil@gmail.com>
2026-08-16 20:46:50 -0300

    Tarefa de domingo 1608

--- src/Aula15/Exercicio7.java
+++ src/Aula15/Exercicio7.java
@@ -8,16 +8,41 @@ public class Exercicio7 {
 
         System.out.println("--- Programa de detecção de número e verificar o maior---");
 
-        System.out.println("Informe o primeiro número");
-        float a1 = scanner.nextFloat();
+        System.out.println("Informe o 1º número");
+        float num1 = scanner.nextFloat();
 
-        System.out.println("Informe o segundo número");
-        float a2 = scanner.nextFloat();
+        System.out.println("Informe o 2º número");
+        float num2 = scanner.nextFloat();
 
-        System.out.println("Informe o segundo número");
-        float a3 = scanner.nextFloat();
+        System.out.println("Informe o 3º número");
+        float num3 = scanner.nextFloat();
 
-        if (a1 > a2) {
+
+        // Inicializa o maior e o menor com o primeiro valor
+        double maior = num1;
+        double menor = num1;
+
+        // Verificação do maior número
+        if (num2 > maior) {
+            maior = num2;
+        }
+        if (num3 > maior) {
+            maior = num3;
+        }
+
+        // Verificação do menor número
+        if (num2 < menor) {
+            menor = num2;
+        }
+        if (num3 < menor) {
+            menor = num3;
+        }
+
+        // Exibição do resultado
+        System.out.println("O maior número é: " + maior);
+        System.out.println("O menor número é: " + menor);
+
+        /*if (a1 > a2) {
             System.out.println("O número é maior");
         } else if (a2 > a3) {
             System.out.println("O número é maior");
@@ -35,7 +60,7 @@ public class Exercicio7 {
             System.out.println("O número é menor");
         } else {
             System.out.println("O número é menor");
-        }
+        }*/
 
     }
 
